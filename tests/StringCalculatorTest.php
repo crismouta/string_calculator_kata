26f3913bb8dd7519d777ff6da999fd037f3d3fa7
crismouta <82060703+crismouta@users.noreply.github.com>
2023-02-22 19:47:18 +0100

    red:test_return_0_when_empty_string

--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -7,5 +7,11 @@ use PHPUnit\Framework\TestCase;
 
 class StringCalculatorTest extends TestCase
 {
-    
+    public function test_return_0_when_empty_string() {
+      $string = new StringCalculator();
+
+      $result = $string->start("");
+      
+      $this->assertEquals(0, $result);
+    }
 }
